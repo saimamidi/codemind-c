0efcc63e7010f4dd69045259751d3d54e8deed2a
saimamidi <147472798+saimamidi@users.noreply.github.com>
2023-10-20 13:40:59 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++){         for(int a=1;a<=i;a++){             printf("*");         }         printf(" ");     }
    }

---
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int a=1;a<=i;a++){
+            printf("*");
+        }
+        printf("
+");
+    }
+    
+}
