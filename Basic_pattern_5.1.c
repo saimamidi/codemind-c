7d4cbd91304435e06c4ef91deef3666ad91d5882
saimamidi <147472798+saimamidi@users.noreply.github.com>
2023-10-30 17:50:00 +0530

    #include<stdio.h>
        int main(){
            int n;
            scanf("%d",&n);
            for(int i=0;i<n;i++){
                for(int a=0;a<=i;a++){
                    printf("%d",i+1);
                }
                for(int b=0;b<(n-i)-1;b++){
                    printf("*");
                }
                printf("
    ");
            }
        }

---
+++ Basic_pattern_5.1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+    int main(){
+        int n;
+        scanf("%d",&n);
+        for(int i=0;i<n;i++){
+            for(int a=0;a<=i;a++){
+                printf("%d",i+1);
+            }
+            for(int b=0;b<(n-i)-1;b++){
+                printf("*");
+            }
+            printf("
+");
+        }
+    }
