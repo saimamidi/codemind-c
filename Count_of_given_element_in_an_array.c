4d3a3ee119a0f6fc744a748edad786e9f8b62880
saimamidi <147472798+saimamidi@users.noreply.github.com>
2023-12-09 15:54:34 +0530

    #include<stdio.h> int main(){     int n,c,count=0;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++){         scanf("%d",&a[i]);     }     scanf("%d",&c);     for(int b=0;b<n;b++){         if(a[b]==c){             count++;         }     }     printf("%d",count); }

---
+++ Count_of_given_element_in_an_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n,c,count=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&c);
+    for(int b=0;b<n;b++){
+        if(a[b]==c){
+            count++;
+        }
+    }
+    printf("%d",count);
+}
