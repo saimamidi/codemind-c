bdfcf5312ed1e0dfe0b371898145df68a98b4cce
saimamidi <147472798+saimamidi@users.noreply.github.com>
2024-03-13 18:29:31 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int arr[n],a[n];     for(int i=0;i<n;i++){         scanf("%d",&arr[i]);     }     int k=0;     for(int i=0;i<n;i++){         if(arr[i]%2!=0){             a[k]=arr[i];             k++;         }     }         for(int i=0;i<n;i++){         if(arr[i]%2==0){             a[k]=arr[i];             k++;         }        }     for(int i=0;i<n;i++){         printf("%d ",a[i])  ;     } }

---
+++ Array_of_odd_and_even_elements.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n],a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int k=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]%2!=0){
+            a[k]=arr[i];
+            k++;
+        }
+    }
+        for(int i=0;i<n;i++){
+        if(arr[i]%2==0){
+            a[k]=arr[i];
+            k++;
+        }
+       }
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i])  ;
+    }
+}
